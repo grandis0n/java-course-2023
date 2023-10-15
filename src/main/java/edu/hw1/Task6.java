67e55a0be38ce54f213ca106a6323cf02eddce3b
Ilya Bizimov <ilya.bizimov@mail.ru>
2023-10-15 20:26:53 +1000

    Updated task 6

--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -6,22 +6,23 @@ public class Task6 {
     private Task6() {
     }
 
-    @SuppressWarnings("MagicNumber")
     public static int countK(int number) {
-        if (number == 6174) {
+        final int numericalSystem = 10;
+        final int KaprecalNumber = 6174;
+        if (number == KaprecalNumber) {
             return 0;
         }
         int tempNumber = number;
         int reversedNumber = 0;
-        int[] digitsArray = new int[4];
-        for (int i = 0; i < 4; i++) {
-            digitsArray[i] = tempNumber % 10;
-            tempNumber /= 10;
+        int[] digitsArray = new int[Integer.toString(number).length()];
+        for (int i = 0; i < digitsArray.length; i++) {
+            digitsArray[i] = tempNumber % numericalSystem;
+            tempNumber /= numericalSystem;
         }
         Arrays.sort(digitsArray);
-        for (int i = 0; i < 4; i++) {
-            tempNumber = tempNumber * 10 + digitsArray[i];
-            reversedNumber = reversedNumber * 10 + digitsArray[3 - i];
+        for (int i = 0; i < digitsArray.length; i++) {
+            tempNumber = tempNumber * numericalSystem + digitsArray[i];
+            reversedNumber = reversedNumber * numericalSystem + digitsArray[digitsArray.length - 1 - i];
         }
         int answer = reversedNumber - tempNumber;
         return 1 + countK(answer);
